e7027b6fa548a1336aa5f429e4df69d84dd0f13d
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2021-12-12 20:50:52 +0000

    50% of homepage design done
    adjust for mobile version in a next week

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import img_home from '../pages/img_home.jpg';
+import '../styles/style.css'
 
 const Home = () => {
 return (
@@ -11,6 +12,8 @@ return (
 		height: '100vh'
 	}}
 	>
+	<div className='homeContent'>
+		
 	<h1>GCS COLLEGE</h1>
 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting 
 	industry. Lorem Ipsum has been the industry's standard dummy text ever
@@ -22,7 +25,10 @@ return (
 	  and more recently with desktop publishing software like Aldus PageMaker
 	   including versions of Lorem Ipsum.	
 	</p>
-	<h2>About us</h2>
+	
+	<img src={img_home} width='50%' height='auto' />
+
+	<h1>About us</h1>
 	<p>Lorem Ipsum is simply dummy text of the printing and typesetting 
 	industry. Lorem Ipsum has been the industry's standard dummy text ever
 	since the 1500s, when an unknown printer took a galley of type and 
@@ -33,7 +39,7 @@ return (
 	  and more recently with desktop publishing software like Aldus PageMaker
 	   including versions of Lorem Ipsum.	
 	</p>
-	<img src={img_home} />
+	</div>
 	</div>
 	
 );
