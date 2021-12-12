ab420b181de24bb1548016dc5a5e15c55d0480fe
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2021-12-12 19:35:11 +0000

    student form page with basic components

--- src/pages/studentForm.js
+++ src/pages/studentForm.js
@@ -1,6 +1,15 @@
-import React from 'react';
+import React, {useState} from 'react';
+import Select from 'react-select'
 
 const StudentForm = () => {
+
+	const [name, setName] = useState("");
+	const [address, setAddress] = useState("");
+	const [email, setEmail] = useState("");
+	const [phone, setPhone] = useState("");
+	const [country, setCountry] = useState("");
+	const [comments, setComments] = useState("");
+
 return (
 	<div
 	style={{
@@ -10,7 +19,38 @@ return (
 		height: '100vh'
 	}}
 	>
-	<h1>GeeksforGeeks is a Computer Science portal for geeks.</h1>
+<div className="login-wrapper">
+      <h1>Student form</h1>
+      <form>
+        <label>
+          <p>Name</p>
+          <input type="text" onChange={e => setName(e.target.value)}/>
+        </label>
+		<label>
+          <p>Address</p>
+          <input type="text" onChange={e => setAddress(e.target.value)}/>
+        </label>
+		<label>
+          <p>email</p>
+          <input type="text" onChange={e => setEmail(e.target.value)}/>
+        </label>
+        <label>
+          <p>phone</p>
+          <input type="text" onChange={e => setPhone(e.target.value)}/>
+        </label>
+		<label>
+          <p>country</p>
+          <input type="text" onChange={e => setCountry(e.target.value)}/>
+        </label>
+		<label>
+          <p>comments</p>
+          <input type="text" onChange={e => setComments(e.target.value)}/>
+        </label>
+        <div>
+          <button type="submit">Submit</button>
+        </div>
+      </form>
+    </div>
 	</div>
 );
 };
