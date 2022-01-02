6ab2acfb9587cf1c93912d5c03b3895049dee0e5
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2022-01-02 23:48:33 +0000

    courses page created

---
+++ src/pages/CourseCreate.js
@@ -0,0 +1,177 @@
+import React, { useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import TextField from '@material-ui/core/TextField';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
+
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+    },
+    avatar: {
+        margin: theme.spacing(1),
+        backgroundColor: theme.palette.secondary.main,
+    },
+    form: {
+        width: '100%', // Fix IE 11 issue.
+        marginTop: theme.spacing(3),
+    },
+    submit: {
+        margin: theme.spacing(3, 0, 2),
+    },
+}));
+const UserCreate = () => {
+    const classes = useStyles();
+
+    function submitStudent() {
+        fetch('http://localhost:8080/api/students/create', {
+            method: 'POST',
+            headers: {
+                Accept: 'application/form-data',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                "name": name,
+                "surname": surname,
+                "address": address,
+                "phoneNumber": phoneNumber,
+                "studentComments": studentComments,
+                "country": country,
+                "password": password,
+                "email": email
+            }),
+        })
+        //need to fix the json answer
+            .then(res => res.json())
+            .then(
+                (result) => {
+                    alert(result['message'])
+                    if (result['status'] === 'ok') {
+                        window.location.href = '../src/components/Users';
+                    }
+                }
+            )
+    }
+
+    const [name, setName] = useState('');
+    const [surname, setSurname] = useState('');
+    const [address, setAddress] = useState('');
+    const [phoneNumber, setPhoneNumber] = useState('');
+    const [country, setCountry] = useState('');
+    const [studentComments, setStudentComments] = useState('');
+    const [password, setPassword] = useState('');
+    const [email, setEmail] = useState('');
+    return (
+        <Container maxWidth="xs">
+            <div className={classes.paper}>
+                <Typography component="h1" variant="h5">
+                    User
+                </Typography>
+                <form className={classes.form}>
+                    <Grid container spacing={2}>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                autoComplete="name"
+                                name="name"
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="name"
+                                label="name"
+                                onChange={(e) => setName(e.target.value)}
+                                autoFocus
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="surname"
+                                label="surname"
+                                onChange={(e) => setSurname(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="address"
+                                label="address"
+                                onChange={(e) => setAddress(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="phoneNumber"
+                                label="phoneNumber"
+                                onChange={(e) => setPhoneNumber(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="country"
+                                label="country"
+                                onChange={(e) => setCountry(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="studentComments"
+                                label="studentComments"
+                                onChange={(e) => setStudentComments(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="password"
+                                label="password"
+                                onChange={(e) => setPassword(e.target.value)}
+                            />
+                        </Grid>
+                        <Grid item xs={12} sm={6}>
+                            <TextField
+                                variant="outlined"
+                                required
+                                fullWidth
+                                id="email"
+                                label="email"
+                                onChange={(e) => setEmail(e.target.value)}
+                            />
+                        </Grid>
+                    </Grid>
+                    <Button
+                     onClick={submitStudent}
+                        type="submit"
+                        fullWidth
+                        variant="contained"
+                        color="primary"
+                        className={classes.submit}
+                    >
+                        Create
+                    </Button>
+                </form>
+            </div>
+        </Container>
+    );
+}
+
+export default UserCreate;
