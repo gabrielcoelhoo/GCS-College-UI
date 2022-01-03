8d6d5ebfe3c306afc70863c843c03051c893b2cc
gabrielcoelhoo <66799435+gabrielcoelhoo@users.noreply.github.com>
2022-01-03 18:37:26 +0000

    enrolments page created

--- src/App.js
+++ src/App.js
@@ -16,13 +16,17 @@ import Employees from '../src/components/Employees'
 import EmployeeCreate from '../src/pages/EmployeeCreate'
 import EmployeeUpdate from '../src/pages/EmployeeUpdate'
 
+import Enrolments from '../src/components/Enrolments'
+import EnrolmentCreate from '../src/pages/EnrolmentCreate'
+import EnrolmentUpdate from '../src/pages/EnrolmentUpdate'
+
 export default function App() {
 	return (
 		<Router>
 		  <div>
 			<Navbar />
 			<Switch>
-			  <Route exact path='/' component={Users} />
+			  <Route exact path='/users' component={Users} />
 			  <Route exact path='/create' component={UserCreate} />
 			  <Route exact path='/update/:id' component={UserUpdate} />
 
@@ -31,8 +35,12 @@ export default function App() {
 			  <Route exact path='/courseupdate/:id' component={CourseUpdate} />
 
 			  <Route exact path='/employees' component={Employees} />
-			  <Route exact path='/createEmployee' component={EmployeeCreate} />
-			  <Route exact path='/employeeUpdate/:id' component={EmployeeUpdate} />
+			  <Route exact path='/employeecreate' component={EmployeeCreate} />
+			  <Route exact path='/employeeupdate/:id' component={EmployeeUpdate} />
+
+			  <Route exact path='/enrolments' component={Enrolments} />
+			  <Route exact path='/enrolmentcreate' component={EnrolmentCreate} />
+			  <Route exact path='/enrolmentupdate/:id' component={EnrolmentUpdate} />
 			</Switch>
 		  </div>
 		</Router>
